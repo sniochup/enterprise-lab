e4c6cd35573595d75b81e95cd1c77b9f2a09f608
sniochup <snioszekpawel@gmail.com>
2023-03-18 20:52:58 +0100

    Lab 1 | Servlet Java, JSP, JSTL solution

---
+++ ServletsJSPJSTL/src/main/java/ai/servlets/ControllerServlet.java
@@ -0,0 +1,53 @@
+package ai.servlets;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import ai.beans.ColorBean;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+@WebServlet(name = "ControllerServlet", urlPatterns = {"/ControllerServlet"})
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ArrayList members = new ArrayList();
+        members.add("John Lennon");
+        members.add("Paul McCartney");
+        members.add("Ringo Starr");
+        members.add("George Harrison");
+        request.setAttribute("members", members);
+        Calendar calDate = new GregorianCalendar();
+        calDate.set(1965, Calendar.SEPTEMBER, 13);
+        Date releaseDate = calDate.getTime();
+        request.setAttribute("releaseDate", releaseDate);
+
+        ColorBean myBean = new ColorBean();
+        myBean.setForegroundColor(request.getParameter("foreColor"));
+        myBean.setBackgroundColor(request.getParameter("backColor"));
+        request.setAttribute("bean", myBean);
+
+        request.setAttribute(
+                "border",
+                "on".equals(request.getParameter("border")) ? "border" : "");
+
+        ServletContext ctx = this.getServletContext();
+        RequestDispatcher dispatcher
+                = ctx.getRequestDispatcher("/yesterday.jsp");
+        dispatcher.forward(request, response);
+    }
+}
