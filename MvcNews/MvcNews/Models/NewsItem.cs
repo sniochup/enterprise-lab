781a01467d1b69bda88dee15ac51895e158b1e15
sniochup <snioszekpawel@gmail.com>
2023-06-13 20:04:38 +0200

    Lab 7 | ASP.NET MVC + Entity Framework (Code First) solution

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+        public int Id { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime TimeStamp { get; set; }
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; } = "";
+    }
+}
